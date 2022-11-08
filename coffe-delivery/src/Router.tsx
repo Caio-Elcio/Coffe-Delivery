9895da0c26bb747d0caa685cbe20e1619629901e
Caio-Elcio <eloyduartecaio@gmail.com>
2022-11-08 10:46:17 -0300

    Front-End Finalizado

--- coffe-delivery/src/Router.tsx
+++ coffe-delivery/src/Router.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Default } from "./layouts/Default"
 import { CompleteOrder } from "./pages/CompleteOrder";
 import { Home } from "./pages/Home";
+import { OrderConfirmedPage } from "./pages/OrderConfirmed";
 
 export function Router() {
     return (
@@ -9,6 +10,7 @@ export function Router() {
             <Route path="/" element={<Default />} >
                 <Route path="/" element={<Home />} />
                 <Route path="/completeOrder" element={<CompleteOrder />} />
+                <Route path="/orderConfirmed" element={<OrderConfirmedPage />} />
             </Route >
         </Routes >
     );
